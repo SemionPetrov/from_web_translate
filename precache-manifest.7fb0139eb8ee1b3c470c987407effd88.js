581acc32647746a77242cdc8564e77a2deeb36b3
SemionPetrov <semen8952390@gmail.com>
2024-12-26 12:21:09 +0300

    я смог

---
+++ precache-manifest.7fb0139eb8ee1b3c470c987407effd88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ffd7b33062345cd602083cddba46ae4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60c6d8b6728d30f7bcaa",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "fa2d8ee02cf184fc4314",
+    "url": "/static/js/2.3ad75f56.chunk.js"
+  },
+  {
+    "revision": "60c6d8b6728d30f7bcaa",
+    "url": "/static/js/main.650ed171.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
